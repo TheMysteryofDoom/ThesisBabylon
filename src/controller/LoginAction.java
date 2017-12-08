988ec0f7714f7f9cc9c462a76d425d69fddbe048
markvticzon <markvticzon@gmail.com>
2017-12-08 13:51:43 +0800

    LoginAction updated

--- src/controller/LoginAction.java
+++ src/controller/LoginAction.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class LoginAction {
+import org.hibernate.mapping.Map;
 
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+	
+	public String execute() throws Exception{
+		if(username){
+			Map session = (Map) ActionContext.getContext().getSession();
+			return SUCCESS;
+		}else{
+			return ERROR;
+		}
+	}
+	public String logout() throws Exception{
+		Map session = (Map) ActionContext.getContext().getSession();
+		return SUCCESS;
+	}
 }
