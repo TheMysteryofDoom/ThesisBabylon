90b3b90307538bf957c0de4ee4eb2fb9755a38d9
markvticzon <markvticzon@gmail.com>
2017-12-17 10:26:10 +0800

    error code fixed

--- src/utility/HibernateUtil.java
+++ src/utility/HibernateUtil.java
@@ -4,22 +4,27 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.*;
+
 
 public class HibernateUtil {
 	 private static SessionFactory sessionFactory;
+	 private static ServiceRegistry serviceRegistry;
 	 
-	    public static SessionFactory getSessionFactory() {
+	 public static SessionFactory getSessionFactory() {
 	        if (sessionFactory == null) {
 	            // loads configuration and mappings
-	            Configuration configuration =  new Configuration().configure();
-	            ServiceRegistry serviceRegistry
-	                    = new StandardServiceRegistryBuilder()
-	                    .applySettings(((Object) configuration).getProperties()).build();
+	            Configuration configuration = new Configuration().configure();
+	       //             = new StandardServiceRegistryBuilder()
+	         //           .applySettings(configuration.getProperties()).build();
+	            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+	                    configuration.getProperties()).build();
 	 
 	            // builds a session factory from the service registry
-	            sessionFactory = ((Object) configuration).buildSessionFactory(serviceRegistry);
+	    //        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	          //  sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 	        }
+	 
+	        
 	 
 	        return sessionFactory;
 	    }
