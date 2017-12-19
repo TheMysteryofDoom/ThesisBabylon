d19fe5711fb20e40d7abc4e5c6fe77e4ba6c1a29
markvticzon <markvticzon@gmail.com>
2017-12-20 01:13:30 +0800

    Code ready check connection.

--- src/controller/LoginAction.java
+++ src/controller/LoginAction.java
@@ -1,50 +1,55 @@
 package controller;
+import java.util.Map;
+
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
+import org.hibernate.Session;
+
 import model.AccessBean;
 import utility.*;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LoginAction extends ActionSupport{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	AccessBean access;
-	User user = new User(); 
-	//Users userLogin;
-	@Override
-	public void validate(){
-		try{
-			if(access.getUsername().length()==(0)){
-				this.addFieldError("access.username", "Username is required");
-			}
-			if(access.getPassword().length()==(0)){
-				this.addFieldError("access.password", "Password is required");
-			}
-		}catch(NullPointerException npe){
-			System.err.println("NULL!");
-		}
-		
-	}
-	@Override
-	public String execute(){
-		if(user.find(access.getUsername(), access.getPassword())){
-			return SUCCESS;
-		}else{
-			this.addActionError("Invalid username and password");
-		}
-		return INPUT;
-	}
+public class LoginAction implements SessionAware{
+	SessionMap<String,String> sessionmap;  
+	AccessBean user = new AccessBean();
+	private String username,password;  
+	public String getUsername() {  
+	    return username;  
+	}  
+	  
+	public void setUsername(String username) {  
+	    this.username = username;  
+	}  
+	  
+	public String getUserpass() {  
+	    return password;  
+	}  
+	  
+	public void setUserpass(String password) {  
+	    this.password = password;  
+	}  
+	  
+	public String execute(){  
+	    if(User.validate(username, password)){  
+	        return "success";  
+	    }  
+	    else{  
+	        return "error";  
+	    }  
+	}  
 	
 	
 	
-	public AccessBean getUser() {
-        return access;
-    }
- 
-    public void setUser(AccessBean access) {
-        this.access = access;
-    }    
-	
+	public void setSession(Map map) {  
+	    sessionmap=(SessionMap)map;  
+	    sessionmap.put("login","true");  
+	}  
+	  
+	public String logout(){  
+	    sessionmap.invalidate();  
+	    return "success";  
+	}  
 	
 	
 }
--- src/utility/User.java
+++ src/utility/User.java
@@ -1,26 +1,33 @@
 package utility;
 import model.AccessBean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 
 //Session for DB
 public class User {
-	public boolean find(String username, String password){
-		  Session session = HibernateUtil.getSessionFactory().openSession();
-		  //session.beginTransaction();
-		  String sql = " from login u where u.uname=:username and u.pwd=:password";
-	        Query query = session.createQuery(sql);
-	        query.setParameter("username", username);
-	        query.setParameter("password", password);
-	        List<AccessBean> list = query.list();
-	        if (list.size() > 0) {
-	            session.close();
-	            return true;
-	        }
-	        session.close();
-	        return false;
-	    }
+	public static boolean validate(String username,String password){  
+		 boolean status=false;  
+		  try{  
+		   Class.forName("com.mysql.jdbc.Driver");  
+		   Connection con=DriverManager
+				   .getConnection("jdbc:mysql://localhost:3306/codersofbabylon","root","");  
+		     
+		   PreparedStatement ps=con.prepareStatement(  
+		     "Select * from login where username=? and password=?");  
+		   ps.setString(1,username);  
+		   ps.setString(2,password);  
+		   ResultSet rs=ps.executeQuery();  
+		   status=rs.next();  
+		  }catch(Exception e){e.printStackTrace();}  
+		 return status;  
+		}  
 	}
 
 
