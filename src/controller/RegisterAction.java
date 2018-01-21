8588b9cb92cd29a3ba65ae5ae096e1f494eb18fc
markvticzon <markvticzon@gmail.com>
2018-01-21 14:26:19 +0800

    added hibernate for registration

--- src/controller/RegisterAction.java
+++ src/controller/RegisterAction.java
@@ -1,15 +1,16 @@
 package controller;
 
-import javax.transaction.Transaction;
 
-import org.hibernate.SessionFactory;
 
 import model.PatientBean;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
-import freemarker.template.Configuration;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class RegisterAction extends ActionSupport implements ModelDriven<PatientBean>{
 	/**
@@ -21,13 +22,29 @@ public class RegisterAction extends ActionSupport implements ModelDriven<Patient
 	
 	public String execute(){
 		String status = ERROR;
+		SessionFactory sessionFactory = 
+				new Configuration().configure().buildSessionFactory();
+		
+		Session session = null;
 		Transaction transaction = null;
 		try{
 			
 			
 		}catch(Exception e){
-			transaction.rollback();
+			session = sessionFactory.openSession();
+			 if(session!=null){
+				transaction = session.beginTransaction();
+				session.save(pb); //insert into sql statement equivalent
+				transaction.commit();
+			 }else{
+				 System.err.println("session is null");
+			 }
 		}
 		return status;
+	}
+	@Override
+	public PatientBean getModel(){
+		return pb;
+		
 	}
 }
