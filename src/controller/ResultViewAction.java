c66aaf5e5ad9c7b48fc566d6b9be2014af609df0
markvticzon <markvticzon@gmail.com>
2018-02-10 21:30:56 +0800

    Data retrieval

--- src/controller/ResultViewAction.java
+++ src/controller/ResultViewAction.java
@@ -1,5 +1,12 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import model.PatientBean;
+import utility.DBSQLOperation;
+import utility.DBSingletonConnection;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ResultViewAction extends ActionSupport{
@@ -8,7 +15,24 @@ public class ResultViewAction extends ActionSupport{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	//Bean Name
+	Connection connection = DBSingletonConnection.getConnection();
 	
-	///Connection connection = 
+	public String execute(){
+		try{
+			ResultSet patient = DBSQLOperation.retrievePatient(connection);
+			ResultSet address = DBSQLOperation.retrievePatientAddress(connection);
+			ResultSet contact = DBSQLOperation.retrievePatientContact(connection);
+			ResultSet emergency = DBSQLOperation.retrievePatientEmergancyContact(connection);
+			
+			while(patient.next()){
+				//p
+				
+			}
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return SUCCESS;
+	}
 }
---
+++ src/model/ResultBean.java
@@ -0,0 +1,41 @@
+package model;
+
+public class ResultBean {
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String birthMonth;
+	private int birthDay;
+	private int birthYear;
+	private String gender;
+	private String birthPlace;
+	private String citizenship;
+	private String religion;
+	private String civil;
+	//occupation
+	private String job;
+	//patients Status
+	private String status;
+	//ADDRESS
+	private int addressId;
+	
+	private String street;
+	private String city;
+	private String country;
+	private String zipcode;
+	
+	//CONTACT
+	private int  contactId;
+	
+	private String contactNo1;
+	private String contactNo2;
+	private String contactNo3;
+	private String email;
+	//EMERGENCY CONTACT
+	private int emergencyId;
+	
+	private String efirstName;
+	private String elastName;
+	private String emiddleName;
+	private String type;
+}
