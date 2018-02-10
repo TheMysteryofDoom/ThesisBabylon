edf9b016243e9a3a6859f3bf9fe9d07af3cb8244
markvticzon <markvticzon@gmail.com>
2018-02-10 21:32:58 +0800

    Bean

--- src/model/ResultBean.java
+++ src/model/ResultBean.java
@@ -38,4 +38,172 @@ public class ResultBean {
 	private String elastName;
 	private String emiddleName;
 	private String type;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getBirthMonth() {
+		return birthMonth;
+	}
+	public void setBirthMonth(String birthMonth) {
+		this.birthMonth = birthMonth;
+	}
+	public int getBirthDay() {
+		return birthDay;
+	}
+	public void setBirthDay(int birthDay) {
+		this.birthDay = birthDay;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthPlace() {
+		return birthPlace;
+	}
+	public void setBirthPlace(String birthPlace) {
+		this.birthPlace = birthPlace;
+	}
+	public String getCitizenship() {
+		return citizenship;
+	}
+	public void setCitizenship(String citizenship) {
+		this.citizenship = citizenship;
+	}
+	public String getReligion() {
+		return religion;
+	}
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+	public String getCivil() {
+		return civil;
+	}
+	public void setCivil(String civil) {
+		this.civil = civil;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	public String getContactNo1() {
+		return contactNo1;
+	}
+	public void setContactNo1(String contactNo1) {
+		this.contactNo1 = contactNo1;
+	}
+	public String getContactNo2() {
+		return contactNo2;
+	}
+	public void setContactNo2(String contactNo2) {
+		this.contactNo2 = contactNo2;
+	}
+	public String getContactNo3() {
+		return contactNo3;
+	}
+	public void setContactNo3(String contactNo3) {
+		this.contactNo3 = contactNo3;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getEmergencyId() {
+		return emergencyId;
+	}
+	public void setEmergencyId(int emergencyId) {
+		this.emergencyId = emergencyId;
+	}
+	public String getEfirstName() {
+		return efirstName;
+	}
+	public void setEfirstName(String efirstName) {
+		this.efirstName = efirstName;
+	}
+	public String getElastName() {
+		return elastName;
+	}
+	public void setElastName(String elastName) {
+		this.elastName = elastName;
+	}
+	public String getEmiddleName() {
+		return emiddleName;
+	}
+	public void setEmiddleName(String emiddleName) {
+		this.emiddleName = emiddleName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 }
