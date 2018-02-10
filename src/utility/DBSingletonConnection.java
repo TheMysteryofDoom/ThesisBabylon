b48d98aa2c52015d6425ba2b9d10c956d28ba3c1
markvticzon <markvticzon@gmail.com>
2018-02-10 10:25:01 +0800

    retrieval

---
+++ src/utility/DBSingletonConnection.java
@@ -0,0 +1,27 @@
+package utility;
+import java.sql.*;
+
+public class DBSingletonConnection {
+	private static Connection connection;
+	
+	private DBSingletonConnection(){
+		
+	}
+	
+	private static Connection getDBConnection(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/codersofbabylon","root","");
+			
+		}catch(ClassNotFoundException nfe){
+			System.err.println(nfe.getMessage());
+		}catch(SQLException sqle){
+			System.err.println(sqle.getMessage());
+		}
+		return connection;
+	}
+	public static Connection getConnection(){
+		return ((connection ==null)?getDBConnection():connection);
+	}
+}
