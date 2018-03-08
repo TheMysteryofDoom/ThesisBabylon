e889a3d8ea1e489e8c58e8ad86c9781d927d5bd5
markvticzon <markvticzon@gmail.com>
2018-03-08 22:55:20 +0800

    Validate(Login)

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -5,54 +5,102 @@ import org.apache.struts2.dispatcher.SessionMap;
 import org.apache.struts2.interceptor.SessionAware;
 //import org.hibernate.Session;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.*;
+
+
+
+import com.opensymphony.xwork2.ActionSupport;
+
 //import model.AccessBean;
-import utility.*;
+//import utility.*;
+import model.*;
 
 //import com.opensymphony.xwork2.ActionSupport;
 
-public class Login implements SessionAware{
-	SessionMap<String,String> sessionmap;  
-	
-	private String username;
-	private String password;  
-	public String getUsername() {  
-	    return username;  
-	}  
-	  
-	public void setUsername(String username) {  
-	    this.username = username;  
-	}  
-	  
+public class Login extends ActionSupport implements SessionAware{
+	SessionMap<String,String> sessionmap;
 
-	  
-	public String getPassword() {
-		return password;
-	}
+	 private String userId,userPassword;
+	Session session = null;
+	String returnResult = "";
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
-	public String execute(){  
-		System.out.println(username);
-	    sessionmap.put("username", username);
-	    if(User.validate(username, password)){  
-	        return "success";  
-	    }  
-	    else{  
-	        return "error";  
-	    }  
-	}  
-	
+		public String execute(){
+			User loginUser = null;
+			try{
+				Configuration configuration = new Configuration();
+				configuration.configure("user_Authentication/hibernate.cfg.xml");
+				StandardServiceRegistryBuilder regBuilder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+				SessionFactory sessionFactory = configuration.buildSessionFactory(regBuilder.build());
+				session = sessionFactory.openSession();
+
+				if(session.isConnected()){
+					System.out.println("Connection success");
+				}else{
+					System.out.println("Connection failed");
+				}
+
+				try{
+					loginUser = (User)session.load(User.class, userId.trim());
+					if(!loginUser.equals(null)){
+						if(userPassword.equals(loginUser.getUserPassword())){
+							returnResult = "success";
+						}
+					}else{
+						returnResult = "error";
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			//System.out.println(username);
+		//    sessionmap.put("username", username);
+		 //   if(User.validate(username, password)){
+		  //      return "success";
+		//    }
+		//    else{
+		//        return "error";
+	//	    }
+			
+			return returnResult;
+		}
+
+
+	public String getUserId() {
+			return userId;
+		}
+
+
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+
+
+		public String getUserPassword() {
+			return userPassword;
+		}
+
+
+		public void setUserPassword(String userPassword) {
+			this.userPassword = userPassword;
+		}
+
+
 	public void setSession(Map map) {  //This happens before execute
-	    sessionmap=(SessionMap)map;  
+	    sessionmap=(SessionMap)map;
 	    sessionmap.put("login","true");
-	}  
-	  
-	public String logout(){  
-	    sessionmap.invalidate();  
-	    return "success";  
-	}  
-	
-	
+	}
+
+	public String logout(){
+	    sessionmap.invalidate();
+	    return "success";
+	}
+
+
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,18 +1,57 @@
 package model;
 
 public class User {
+	private String adminId;
 	private String username;
-	private String password;
+	private String userPassword;
+	private String email;
+	private String phoneNumber;
+	private String status;
+	
+	
 	public User(){
 		
 	}
+	
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public User(String username){
 		this.username = username;
 	}
 	
 	public User(String username, String password){
 		this.username = username;
-		this.password = password;
+		this.userPassword = password;
 	}
 	public String getUsername() {
 		return username;
@@ -21,12 +60,15 @@ public class User {
 		this.username = username;
 
 	}
-	public String getPassword() {
-		return password;
+
+	public String getUserPassword() {
+		return userPassword;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
 	}
+
 	//USER
 	
 	
