2c29c2870d87abc8bc749a5378bff8b2c33927a7
markvticzon <markvticzon@gmail.com>
2018-03-16 22:26:55 +0800

    Action class updated, with fetch from model.

--- src/controller/PrintAction.java
+++ src/controller/PrintAction.java
@@ -1,36 +1,53 @@
 package controller;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+//import java.io.ByteArrayInputStream;
+//import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
 
 import model.PatientBean;
-import utility.SQLCommand;
+//import utility.SQLCommand;
+
 import com.itextpdf.text.Document;
-import com.itextpdf.text.Image;
+//import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.codec.Base64.InputStream;
+//import com.itextpdf.text.pdf.codec.Base64.InputStream;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
-
+//Use ItextPDF Jar file.
 public class PrintAction extends ActionSupport implements ModelDriven<PatientBean>{
-	private InputStream inputStream;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//private InputStream inputStream;
 	
+	private PatientBean pb = new PatientBean();
+	@Override
 	public String execute(){
 		String status = ERROR;
 		Document document = new Document(); 
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		Connection conn = null;
+		//ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		//Connection conn = null;
 		
 		try{
-			//Database connection 
+		/*	//Database connection 
 			//String URL = "jdbc:mysql://localhost:3306/codersofbabylon";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/codersofbabylon","","");
-			//from Utility - SQLCommand
-			PreparedStatement ps = conn.prepareStatement(GET_PATIENT);
+			// UPDATE DB CONNECTION ONCE HASHED.
+			String sql = "SELECT firstName FROM patientregistry WHERE";
+			sql += "lastName = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()){
+				name = 
+			}
 			PdfWriter.getInstance(document, buffer);
 			
 			document.open();
@@ -42,23 +59,33 @@ public class PrintAction extends ActionSupport implements ModelDriven<PatientBea
 			document.close();
 			document.close(); 
 
-		
-			inputStream  =  new ByteArrayInputStream(buffer.toByteArray());
+			
+			inputStream  =  new ByteArrayInputStream(buffer.toByteArray()); */
+			String firstName = pb.getFirstName();
+			PdfWriter.getInstance(document, new FileOutputStream("e:\\Sample_Output.pdf"));
+			document.open();
+			document.add(new Paragraph("TEST PRINTING"));
+			document.add(new Paragraph(firstName));
+			document.close();
+			
+			status = SUCCESS;
+			
 			
 			
 		}catch(Exception e){
+			status = ERROR;
 			e.printStackTrace();
 		}
 		return status;
 	}
 
-	public InputStream getInputStream() {
+/*	public InputStream getInputStream() {
 		return inputStream;
 	}
 
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
-	}
+	} */
 
 	@Override
 	public PatientBean getModel() {
--- src/controller/ViewPDFAction.java
+++ src/controller/ViewPDFAction.java
@@ -2,6 +2,9 @@ package controller;
 import java.io.ByteArrayOutputStream;
 
 
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts2.interceptor.ServletResponseAware;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -11,12 +14,18 @@ public class ViewPDFAction extends ActionSupport
 	
 	public String exectute(){
 		try{
-			ByteArrayOutputStream baosPDF = new PDFGenerator()
-			.generatePDF();
+		//	ByteArrayOutputStream baosPDF = new PDFGenerator()
+		//	.generatePDF();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return NONE;
+	}
+
+	@Override
+	public void setServletResponse(HttpServletResponse arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
