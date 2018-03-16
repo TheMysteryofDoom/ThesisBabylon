819fdbbbb28bf8869ee166007a00344d1c64a3f2
Isaac Jaranilla <isaacglenn.jaranilla@yahoo.com>
2018-03-16 07:18:08 +0300

    pushing first

---
+++ src/controller/RegisterAdmin.java
@@ -0,0 +1,10 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import model.EmergencyContactBean;
+
+public class RegisterAdmin extends ActionSupport implements ModelDriven<User> {
+
+}
