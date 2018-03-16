28fd7a2afd90f159c47000c9f0f5b6f2ccff1fdc
markvticzon <markvticzon@gmail.com>
2018-03-16 13:57:36 +0800

    Action class updated

--- src/controller/PrintAction.java
+++ src/controller/PrintAction.java
@@ -1,27 +1,57 @@
 package controller;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
+
+import model.PatientBean;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.codec.Base64.InputStream;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 
-public class PrintAction {
-	
+public class PrintAction extends ActionSupport  implements ModelDriven<PatientBean>{
+	private InputStream inputStream;
 	
 	public String execute(){
-		Document document = new Document();
+		String status = ERROR;
+		Document document = new Document(); 
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
 		
 		try{
-			PdfWriter.getInstance(document,
-					new FileOutputStream("image"));
+			PdfWriter.getInstance(document, buffer);
 			
-			Image ID = Image.getInstance("");
-			document.add("");
+			document.open();
+			Paragraph p = new Paragraph();
+			//import here The Name of the User from Database.
+			p.add("MARK LUIS GABRIEL V. TICZON");
 			
+			document.add(p);
+			document.close();
 			document.close(); 
+
+		
+			inputStream = new ByteArrayInputStream(buffer.toByteArray());
+			
+			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		return status;
 	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+	
+	
 	
 }
---
+++ src/controller/ViewPDFAction.java
@@ -0,0 +1,22 @@
+package controller;
+import java.io.ByteArrayOutputStream;
+
+
+import org.apache.struts2.interceptor.ServletResponseAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ViewPDFAction extends ActionSupport
+	implements ServletResponseAware{
+	
+	public String exectute(){
+		try{
+			ByteArrayOutputStream baosPDF = new PDFGenerator()
+			.generatePDF();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return NONE;
+	}
+	
+}
