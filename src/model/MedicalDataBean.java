3bf29af37ccd1505951924bbd627addf81f1efe7
markvticzon <markvticzon@gmail.com>
2018-03-22 13:43:44 +0800

    Made persistence

--- src/model/MedicalDataBean.java
+++ src/model/MedicalDataBean.java
@@ -2,10 +2,28 @@ package model;
 
 import java.io.File;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "patientresults")
 public class MedicalDataBean {
+	@Id
+	@GeneratedValue
+	private int fileId;
+	
 	private File fileUpload;
 	private String fileUploadContentType;
 	private String fileUploadFileName;
+	
+	public int getFileId() {
+		return fileId;
+	}
+	public void setFileId(int fileId) {
+		this.fileId = fileId;
+	}
 	public File getFileUpload() {
 		return fileUpload;
 	}
