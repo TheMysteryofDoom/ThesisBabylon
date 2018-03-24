7f04448bb89c77c3ded5dc0510f1550e6e662a8d
markvticzon <markvticzon@gmail.com>
2018-03-24 08:52:33 +0800

    Merging

--- src/model/VitalSignBean.java
+++ src/model/VitalSignBean.java
@@ -1,7 +1,9 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 @Entity
-@Table(name = "")
+@Table(name = "patientVitalSign")
 public class VitalSignBean {
-
+//NO ENTRIES YET CHECK ONE TO MANY ANNOTATION
 }
