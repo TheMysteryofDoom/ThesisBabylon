2976104aacf119a77147fdf27eb053d1bce5147a
markvticzon <markvticzon@gmail.com>
2018-04-05 21:21:13 +0800

    updated Contact Bean

--- src/model/PatientContactBean.java
+++ src/model/PatientContactBean.java
@@ -1,8 +1,10 @@
 package model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -12,13 +14,23 @@ public class PatientContactBean {
 	
 	@Id
 	@GeneratedValue
+	@Column(name = "CONTACT_ID", unique = true, nullable = false)
 	private int  contactId;
 	
+	@Column(name = "CONTACT_NO1", unique = false, nullable = false)
 	private String contactNo1;
+	
+	@Column(name = "CONTACT_NO2", unique = false, nullable = true)
 	private String contactNo2;
+	
+	@Column(name = "CONTACT_NO3", unique = false, nullable = true)
 	private String contactNo3;
+	
+	@Column(name = "EMAIL", unique = false, nullable = false)
 	private String email;
 	
+	@ManyToOne
+	private PatientBean pb;
 	
 	public int getContactId() {
 		return contactId;
@@ -49,6 +61,12 @@ public class PatientContactBean {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public PatientBean getPb() {
+		return pb;
+	}
+	public void setPb(PatientBean pb) {
+		this.pb = pb;
 	}
 	
 
