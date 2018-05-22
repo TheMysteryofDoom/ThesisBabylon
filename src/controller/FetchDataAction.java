26d274c68fb4c85499cdb97fcb595c7a7b83688f
markvticzon <markvticzon@gmail.com>
2018-05-22 15:28:44 +0800

    partial implement

---
+++ src/controller/FetchDataAction.java
@@ -0,0 +1,10 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class FetchDataAction extends ActionSupport{
+	List<FetchDataBean> dataList = null;
+	ResultSet rs = null;
+	String fetchData = null;
+	FetchData
+}
