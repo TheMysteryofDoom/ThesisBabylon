7fbf125e8b64c488836f70273f183c025851b841
Mark Luis Gabriel V. Ticzon <201501162@iacademy.loc>
2018-05-26 08:24:27 +0800

    Medical Status action class

---
+++ src/controller/UpdateMedicalStatusAction.java
@@ -0,0 +1,32 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import model.MedicalDataBean;
+
+
+
+public class UpdateMedicalStatusAction extends ActionSupport implements ModelDriven<MedicalDataBean>{
+
+	
+	public String execute(){
+		System.out.println("Medical Status is mapped here.");
+		// get the patientID here from search.
+		
+		
+		return SUCCESS;
+		
+	}
+	
+	
+	
+	@Override
+	public MedicalDataBean getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
